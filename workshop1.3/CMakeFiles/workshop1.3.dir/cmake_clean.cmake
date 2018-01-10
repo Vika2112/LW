6eebbf41ac1d969141f60403f10e22691af7e7e5
vika2112 <vikagrigorieva1@gmail.com>
2018-01-10 13:51:56 +0300

    Выполнила Workshop1.2

---
+++ workshop1.3/CMakeFiles/workshop1.3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/workshop1.3.dir/main.obj"
+  "workshop1.3.pdb"
+  "workshop1.3.exe"
+  "workshop1.3.exe.manifest"
+  "libworkshop1.3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/workshop1.3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
